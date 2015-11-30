ccbf2223d13617c14f1e06fa7a9f3eb974d26f75
Jacek Rzeszutek <rzeszutekjacek@gmail.com>
2015-11-30 23:11:22 +0100

    add basic redis storage

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var config = require('./webpack.config');
 
 var app = new (require('express'))();
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var port = 3000;
@@ -15,29 +15,35 @@ app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output
 app.use(webpackHotMiddleware(compiler));
 
 app.get(["/", "/device/:deviceUuid"], function(req, res) {
-  res.sendFile(__dirname + '/app/index.html');
+    res.sendFile(__dirname + '/app/index.html');
 });
 
-
-var devices = {};
+var redis = require("redis"),
+    redisClient = redis.createClient({
+        prefix: 'watchdog:'
+    });
 
 app.get("/api/device/:deviceUuid", function(req, res) {
-  return res.send(devices[req.params.deviceUuid]);
+    redisClient.get(req.params.deviceUuid, function(err, reply) {
+        res.send(JSON.parse(reply));
+    });
 });
 
-app.post('/api/device/create', function (req, res) {
-  if (!devices[req.body.uuid]) {
-    devices[req.body.uuid] = {
-      owner: req.body.uuid, listeners: []
-    };
-  }
-  res.send('OK');
+app.post('/api/device/create', function(req, res) {
+    var uuid = req.body.uuid,
+        device = {
+            owner: uuid,
+            listeners: []
+        };
+
+    redisClient.set(uuid, JSON.stringify(device));
+    res.send('OK');
 });
 
 app.listen(port, function(error) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
-  }
+    if (error) {
+        console.error(error);
+    } else {
+        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
+    }
 });
