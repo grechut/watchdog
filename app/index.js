01e73f9aad4bd324e839197cda74c488a99630ed
Szymon Nowak <szimek@gmail.com>
2016-02-20 20:27:35 +0100

    Move routes to a separate file

--- app/index.js
+++ app/index.js
@@ -1,68 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import firebase from './lib/firebase';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
 
 require('../node_modules/react-mdl/extra/material.css');
 require('../node_modules/react-mdl/extra/material.js');
 
 require('./styles/main.css');
 
+// TODO: set initial state on the server (?)
+window.__INITIAL_STATE__ = {};
 import configureStore from './store/configureStore';
-const store = configureStore();
+const store = configureStore(window.__INITIAL_STATE__);
 
 import installServiceWorker from './lib/install-sw-push-notifications';
 installServiceWorker(store.dispatch);
 
-import App from './containers/App';
-import LandingPage from './containers/LandingPage';
-import LayoutContainer from './containers/LayoutContainer';
-import DeviceList from './containers/DeviceList';
-import Device from './containers/Device';
-import DeviceOwner from './containers/DeviceOwner';
-import DeviceNew from './containers/DeviceNew';
-
-function requireAuth(nextState, replace) {
-  if (!firebase.getAuth()) {
-    replace({
-      pathname: '/',
-      state: { nextPathname: nextState.location.pathname },
-    });
-  }
-}
-
-function requireNoAuth(nextState, replace) {
-  if (firebase.getAuth()) {
-    replace({
-      pathname: '/devices',
-      state: { nextPathname: nextState.location.pathname },
-    });
-  }
-}
-
-function requireDeviceOwnership() {
-  // TODO implement me
-}
-
 render(
   <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={LandingPage} onEnter={requireNoAuth} />
-
-        <Route path="devices" component={LayoutContainer} onEnter={requireAuth}>
-          <IndexRoute component={DeviceList} />
-          <Route path="new" component={DeviceNew} />
-          <Route path=":deviceUuid" component={Device} />
-          <Route
-            path=":deviceUuid/device"
-            component={DeviceOwner}
-            onEnter={requireDeviceOwnership}
-          />
-        </Route>
-      </Route>
-    </Router>
+    <Router history={browserHistory} routes={routes} />
   </Provider>,
   document.getElementById('root')
 );
---
+++ app/routes.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import firebase from './lib/firebase';
+
+import App from './containers/App';
+import LandingPage from './containers/LandingPage';
+import LayoutContainer from './containers/LayoutContainer';
+import DeviceList from './containers/DeviceList';
+import Device from './containers/Device';
+import DeviceOwner from './containers/DeviceOwner';
+import DeviceNew from './containers/DeviceNew';
+
+function requireAuth(nextState, replace) {
+  if (!firebase.getAuth()) {
+    replace({
+      pathname: '/',
+      state: { nextPathname: nextState.location.pathname },
+    });
+  }
+}
+
+function requireNoAuth(nextState, replace) {
+  if (firebase.getAuth()) {
+    replace({
+      pathname: '/devices',
+      state: { nextPathname: nextState.location.pathname },
+    });
+  }
+}
+
+function requireDeviceOwnership() {
+  // TODO implement me
+}
+
+
+const routes = (
+  <Route path="/" component={App}>
+    <IndexRoute component={LandingPage} onEnter={requireNoAuth} />
+
+    <Route path="devices" component={LayoutContainer} onEnter={requireAuth}>
+      <IndexRoute component={DeviceList} />
+      <Route path="new" component={DeviceNew} />
+      <Route path=":deviceUuid" component={Device} />
+      <Route
+        path=":deviceUuid/device"
+        component={DeviceOwner}
+        onEnter={requireDeviceOwnership}
+      />
+    </Route>
+  </Route>
+);
+
+export default routes;
