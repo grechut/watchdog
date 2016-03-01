6cfdc65b74cea39b211fa6926f9722c0ca1eedb9
Szymon Nowak <szimek@gmail.com>
2016-03-01 14:48:26 +0100

    Show drawer on large screens. Change colors to crappy ones

--- app/containers/LayoutContainer.jsx
+++ app/containers/LayoutContainer.jsx
@@ -1,35 +1,25 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { browserHistory, Link } from 'react-router';
+import { Link } from 'react-router';
 import { Layout, Content, Drawer, Navigation, Header } from 'react-mdl/lib/Layout';
 import Grid, { Cell } from 'react-mdl/lib/Grid';
-import IconButton from 'react-mdl/lib/IconButton';
 import User from '../components/User';
-import SignInSignOut from '../components/SignInSignOut';
+import AuthActions from '../actions/auth';
 
 class LayoutContainer extends React.Component {
   render() {
     const { children, dispatch, page, auth } = this.props;
 
     return (
-      <Layout fixedHeader>
-        <Header title={page.title}>
-          {/* TODO Probably not needed once we have hamburger menu
-          <IconButton
-            name="arrow_back"
-            className="mdl-layout-icon"
-            ripple
-            onClick={ () => browserHistory.goBack() }
-          />*/}
-        </Header>
-        <Drawer fixedDrawer>
-            <Navigation>
+      <Layout fixedHeader fixedDrawer>
+        <Header title={page.title} />
+        <Drawer className="mdl-color--blue-grey-900 mdl-color-text--blue-grey-50">
+            <header className="drawer-header">
               <User auth={auth} />
+            </header>
+            <Navigation className="drawer-navigation mdl-color--blue-grey-800">
               <Link to="/devices">Devices</Link>
-              <SignInSignOut
-                auth={auth}
-                dispatch={dispatch}
-              />
+              <a href="#" onClick={() => dispatch(AuthActions.signOut())}>Sign out</a>
             </Navigation>
           </Drawer>
         <Content>
@@ -47,14 +37,14 @@ class LayoutContainer extends React.Component {
 LayoutContainer.propTypes = {
   children: PropTypes.object,
   dispatch: PropTypes.func,
+  auth: PropTypes.object,
   page: PropTypes.object,
-  auth: PropTypes.auth,
 };
 
 function mapStateToProps(state) {
   return {
-    page: state.page,
     auth: state.auth,
+    page: state.page,
   };
 }
 
