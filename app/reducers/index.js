aa920d4b993a115ad5a61c05637078c927ace93b
Jacek Rzeszutek <rzeszutekjacek@gmail.com>
2016-05-02 19:21:16 +0200

    Remove unused reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,7 +1,6 @@
 import auth from 'reducers/auth';
 import devices from 'reducers/devices';
 import incidents from 'reducers/incidents';
-import page from 'reducers/page';
 import peer from 'reducers/peer';
 import pushNotification from 'reducers/push-notification';
 
@@ -9,7 +8,6 @@ const reducers = {
   auth,
   devices,
   incidents,
-  page,
   peer,
   pushNotification,
 };
