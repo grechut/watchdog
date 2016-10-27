2b3422e341f7e741140ba58e966c9ff56a5b33f5
Jacek Rzeszutek <rzeszutekjacek@gmail.com>
2016-10-28 00:12:44 +0200

    Fix https redirect for heroku

--- app/requireHTTPS.js
+++ app/requireHTTPS.js
@@ -1,7 +1,5 @@
 module.exports = (req, res, next) => {
-  // For Heroku/nginx specific condition we could add
-  // req.get('x-forwarded-proto') !== 'https'
-  if (!req.secure && process.env.NODE_ENV !== "development") {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
     return res.redirect('https://' + req.get('host') + req.url);
   }
   next();
