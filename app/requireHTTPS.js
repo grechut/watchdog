d74f825c0c3428b25d0579f0ec29472fd43efcfb
Szymon Nowak <szimek@gmail.com>
2016-10-31 20:18:37 +0100

    Don't redirect to HTTPS in dev

--- app/requireHTTPS.js
+++ app/requireHTTPS.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-    return res.redirect('https://' + req.get('host') + req.url);
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && req.app.get('env') !== 'development') {
+    return res.redirect(`https://${req.get('host')}${req.url}`);
   }
-  next();
+
+  return next();
 };
