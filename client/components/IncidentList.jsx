6a95a33d5a59ce876506f87756d87e719ffa856b
Jacek Rzeszutek <rzeszutekjacek@gmail.com>
2016-12-05 23:14:41 +0100

    Update relative time on incidents list

--- client/components/IncidentList.jsx
+++ client/components/IncidentList.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Grid, { Cell } from 'react-mdl/lib/Grid';
 import { List, ListItem, ListItemContent } from 'react-mdl/lib/List';
@@ -6,37 +6,48 @@ import { CardText } from 'react-mdl/lib/Card';
 
 import IncidentListItem from '../components/IncidentListItem';
 
-function IncidentList(props) {
-  const { incidents } = props;
-  const incidentIds = Object.keys(incidents);
-  const emptyList = (
-    <List>
-      <ListItem>
-        <ListItemContent>
-          None yet.
-        </ListItemContent>
-      </ListItem>
-    </List>
-  );
-  const nonEmptyList = (
-    <List>
-      {incidentIds.map(id =>
-        <IncidentListItem key={id} incident={incidents[id]} />,
-      )}
-    </List>
-  );
-  const list = incidentIds.length ? nonEmptyList : emptyList;
+class IncidentList extends Component {
+  componentDidMount() {
+    // refresh the relative time once per minute
+    this._refreshInterval = setInterval(this.forceUpdate, 60 * 1000);
+  }
 
-  return (
-    <Grid component={CardText}>
-      <Cell component="h4" col={12}>Incidents</Cell>
-      <Cell col={12}>
-        <div className="incident-list">
-          {list}
-        </div>
-      </Cell>
-    </Grid>
-  );
+  componentWillUnmount() {
+    clearInterval(this._refreshInterval);
+  }
+
+  render() {
+    const { incidents } = this.props;
+    const incidentIds = Object.keys(incidents);
+    const emptyList = (
+      <List>
+        <ListItem>
+          <ListItemContent>
+            None yet.
+          </ListItemContent>
+        </ListItem>
+      </List>
+    );
+    const nonEmptyList = (
+      <List>
+        {incidentIds.map(id =>
+          <IncidentListItem key={id} incident={incidents[id]} />,
+        )}
+      </List>
+    );
+    const list = incidentIds.length ? nonEmptyList : emptyList;
+
+    return (
+      <Grid component={CardText}>
+        <Cell component="h4" col={12}>Incidents</Cell>
+        <Cell col={12}>
+          <div className="incident-list">
+            {list}
+          </div>
+        </Cell>
+      </Grid>
+    );
+  }
 }
 
 IncidentList.propTypes = {
